daaeefb4253e187a3ec9d318d20ce55a85809702
Patricio Lopez Valiente <patriciolopezvaliente@gmail.com>
2025-08-23 23:22:33 -0300

    variable adapt

---
+++ src/model/site-labels.ts
@@ -0,0 +1,10 @@
+export const siteLabels = {
+    banner: '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
+        '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
+        '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
+        '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
+        '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
+        '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
+        '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
+        '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina'
+}
