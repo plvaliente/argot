22d48d0d577d309c84ebf402a5ed07c997746860
Patricio Lopez Valiente <patriciolopezvaliente@gmail.com>
2025-08-24 00:22:42 -0300

    add font  and changes

--- src/model/site-labels.ts
+++ src/model/site-labels.ts
@@ -1,10 +1,20 @@
 export const siteLabels = {
-    banner: '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
-        '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
-        '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
-        '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
-        '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
-        '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
-        '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
-        '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina'
+  instagram: 'https://www.instagram.com/argot.colectivo/',
+  banner: '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
+    '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
+    '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
+    '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
+    '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
+    '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
+    '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina' +
+    '/// @argot.colectivo /// hola@argot.com.ar /// Buenos Aires, Argentina',
+  description: {
+    intro: `En un rincón del mundo donde el cielo parecía no tener fin, donde las 
+    nubes danzaban con la brisa y el horizonte se fundía con la tierra, nació 
+    una idea: ARGOT...`,
+    exploration: `Caminamos entre los pliegues del lenguaje, explorando los matices que 
+    otros descuidan...`,
+    welcome: `Así que bienvenidos. No prometemos verdades absolutas, ni caminos seguros. 
+    Pero sí un espacio de exploración, de juego y de creación...`
+  }
 }
